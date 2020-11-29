b01111fba60063a7e7591920bc148642cd6eacbc
MarnusVorster <marnus.vorster@gmail.com>
2020-11-29 20:07:33 +0200

    1 more change

--- deploy_script.sh
+++ deploy_script.sh
@@ -5,4 +5,4 @@ pip3 install -r requirements.txt
 export PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 export PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 export WEATHER_API_KEY=
-python3 manage.py runserver $PRIVATE_IP:80 &
+python3 manage.py runserver $PRIVATE_IP:80 > /dev/null 2>&1
